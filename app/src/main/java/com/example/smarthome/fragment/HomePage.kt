17c9bebb875dcb69799d760ded46f673ed7584a1
d.derenok <d.derenok@itransition.com>
2020-11-19 01:13:53 +0300

    [SMA-3] Add navigation menu for switch between sensor list, home page and room list pages

---
+++ app/src/main/java/com/example/smarthome/fragment/HomePage.kt
@@ -0,0 +1,8 @@
+package com.example.smarthome.fragment
+
+import androidx.fragment.app.Fragment
+import com.example.smarthome.R.layout.fragment_home_page
+
+class HomePage : Fragment(fragment_home_page) {
+
+}
---
+++ app/src/main/java/com/example/smarthome/fragment/SensorList.kt
@@ -0,0 +1,8 @@
+package com.example.smarthome.fragment
+
+import androidx.fragment.app.Fragment
+import com.example.smarthome.R.layout.fragment_sensor_list
+
+class SensorList : Fragment(fragment_sensor_list) {
+
+}
