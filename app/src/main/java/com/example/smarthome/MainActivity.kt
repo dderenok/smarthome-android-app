54d70e515ee0ba81597f90be031c4d32d83b5d0d
d.derenok <d.derenok@itransition.com>
2020-11-20 23:15:55 +0300

    [SMA-10] Add content for room list navigation page

--- app/src/main/java/com/example/smarthome/MainActivity.kt
+++ app/src/main/java/com/example/smarthome/MainActivity.kt
@@ -2,80 +2,48 @@ package com.example.smarthome
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log.d
 import androidx.fragment.app.Fragment
-import androidx.recyclerview.widget.LinearLayoutManager
-import androidx.recyclerview.widget.RecyclerView
+import com.example.smarthome.R.id.bottomNavigationView
 import com.example.smarthome.R.id.flFragment
-import com.example.smarthome.R.id.recyclerView as recyclerViewLayout
+import com.example.smarthome.R.id.home_page
+import com.example.smarthome.R.id.sensor_list
+import com.example.smarthome.R.id.room_list
 import com.example.smarthome.R.layout.activity_main
-import com.example.smarthome.adapter.RoomAdapter
 import com.example.smarthome.fragment.HomePage
 import com.example.smarthome.fragment.RoomList
 import com.example.smarthome.fragment.SensorList
-import com.example.smarthome.model.Room
 import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
-    private lateinit var recyclerView: RecyclerView
-    private lateinit var viewAdapter: RecyclerView.Adapter<*>
-    private lateinit var viewManager: RecyclerView.LayoutManager
-    private var roomList = listOf<Room>()
-
-    init {
-        roomList = roomList + listOf(Room("Кухня", 3))
-        roomList = roomList + listOf(Room("Ванная", 1))
-        roomList = roomList + listOf(Room("Подвал", 2))
-        roomList = roomList + listOf(Room("Ванная", 1))
-        roomList = roomList + listOf(Room("Подвал", 2))
-    }
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(activity_main)
-        d(MAIN_LOG_TAG, "Main page started")
-
-        val sensorListFragment = SensorList()
-        val homePageFragment = HomePage()
-        val roomListFragment = RoomList()
-
-        setCurrentFragment(sensorListFragment)
-
-        val bottomNavView = findViewById<BottomNavigationView>(R.id.bottomNavigationView)
-        bottomNavView.setOnNavigationItemSelectedListener {
-            when (it.itemId) {
-                R.id.sensor_list->setCurrentFragment(sensorListFragment)
-                R.id.home_page->setCurrentFragment(homePageFragment)
-                R.id.room_list->setCurrentFragment(roomListFragment)
-            }
-            true
-        }
-
-        if (roomList.isEmpty()) {
-            // TODO d.derenok: Add generate room list method
-        }
-
-        performRecyclerView()
+        setNavigationMenuSettings()
     }
 
-    private fun setCurrentFragment(fragment: Fragment) =
-        supportFragmentManager.beginTransaction().apply {
+    private fun setNavigationMenuSettings() {
+        val roomListFragment = RoomList()
+        setCurrentFragment(roomListFragment)
+
+        findViewById<BottomNavigationView>(bottomNavigationView)
+            .let {
+                it.setNavigationItemListener(currentNavigationItem = roomListFragment)
+            }
+    }
+
+    private fun setCurrentFragment(fragment: Fragment) = supportFragmentManager.beginTransaction()
+        .apply {
             replace(flFragment, fragment)
             commit()
         }
 
-    private fun performRecyclerView() {
-        viewManager = LinearLayoutManager(this)
-        viewAdapter = RoomAdapter(roomList)
-
-        recyclerView = findViewById<RecyclerView>(recyclerViewLayout).apply {
-            setHasFixedSize(true)
-            layoutManager = viewManager
-            adapter = viewAdapter
+    private fun BottomNavigationView.setNavigationItemListener(currentNavigationItem: Fragment) =
+        setOnNavigationItemSelectedListener { menuItem ->
+            when (menuItem.itemId) {
+                room_list -> setCurrentFragment(currentNavigationItem)
+                sensor_list -> setCurrentFragment(SensorList())
+                home_page ->setCurrentFragment(HomePage())
+            }
+            true
         }
-    }
-
-    companion object {
-        private var MAIN_LOG_TAG = MainActivity::class.java.simpleName
-    }
 }
--- app/src/main/java/com/example/smarthome/fragment/RoomList.kt
+++ app/src/main/java/com/example/smarthome/fragment/RoomList.kt
@@ -1,8 +1,46 @@
 package com.example.smarthome.fragment
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.example.smarthome.R.id.recyclerView as recyclerViewLayout
 import com.example.smarthome.R.layout.fragment_room_list
+import com.example.smarthome.adapter.RoomAdapter
+import com.example.smarthome.model.Room
 
-class RoomList : Fragment(fragment_room_list) {
+class RoomList : Fragment() {
+    private lateinit var recyclerView: RecyclerView
+    private var roomList = listOf<Room>()
 
+    init {
+        roomList = roomList + listOf(Room("Кухня", 3))
+        roomList = roomList + listOf(Room("Ванная", 1))
+        roomList = roomList + listOf(Room("Подвал", 2))
+        roomList = roomList + listOf(Room("Ванная", 1))
+        roomList = roomList + listOf(Room("Подвал", 2))
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        val view = inflater.inflate(fragment_room_list, container, false)
+
+        if (roomList.isEmpty()) {
+            // TODO d.derenok: Add generate room list method
+        }
+
+        recyclerView = view.findViewById<RecyclerView>(recyclerViewLayout).apply {
+            setHasFixedSize(true)
+            layoutManager = LinearLayoutManager(view.context)
+            adapter = RoomAdapter(roomList)
+        }
+
+        return view
+    }
 }
